e22534c163954331d138176527959241e4c3cc84
Martin Rådmans <mcflander@MacBook-Pro-som-tillhor-Martin.local>
2024-12-04 13:23:48 +0100

    Try to fix so the pagemnu on undersida and undersida 2 to work and look the same. Almost done with it.

--- wp-content/themes/MartinRadmans_laboration1/page.php
+++ wp-content/themes/MartinRadmans_laboration1/page.php
@@ -1,18 +1,49 @@
 <?php get_header(); ?>
 <div class="container mt-5">
   <div class="row">
-    <div class="col-md-8">
-      <?php
-      if (have_posts()) :
-        while (have_posts()) : the_post(); ?>
-          <?php the_content(); ?>
-      <?php endwhile;
-      endif;
-      ?>
-    </div>
-    <div class="col-md-4">
-      <?php get_sidebar(); ?>
-    </div>
+    <?php if (is_page('undersida-2')) : ?>
+      <div id="primary" class="col-xs-12 col-md-9 col-md-push-3">
+        <?php
+        if (have_posts()) :
+          while (have_posts()) : the_post(); ?>
+            <?php the_content(); ?>
+        <?php endwhile;
+        endif;
+        ?>
+      </div>
+      <aside id="secondary" class="col-xs-12 col-md-3 col-md-pull-9">
+        <ul id="menu-sidomeny" class="side-menu">
+          <?php wp_nav_menu(array(
+            'theme_location' => 'pagemenu',
+            'menu_class' => 'menu',
+            'container' => false,
+            'items_wrap' => '%3$s' // This removes the <ul> wrapper added by wp_nav_menu
+          ));
+          ?>
+        </ul>
+      </aside>
+    <?php else : ?>
+      <div id="primary" class="col-xs-12 col-md-9">
+        <?php
+        if (have_posts()) :
+          while (have_posts()) : the_post(); ?>
+            <?php the_content(); ?>
+        <?php endwhile;
+        endif;
+        ?>
+      </div>
+      <aside id="secondary" class="col-xs-12 col-md-3">
+        <ul id="menu-sidomeny" class="side-menu">
+          <?php wp_nav_menu(array(
+            'theme_location' => 'pagemenu',
+            'menu_class' => 'menu',
+            'container' => false,
+            'items_wrap' => '%3$s' // This removes the <ul> wrapper added by wp_nav_menu
+          ));
+          ?>
+        </ul>
+      </aside>
+    <?php endif; ?>
   </div>
 </div>
 <?php get_footer(); ?>
--- wp-content/themes/MartinRadmans_laboration1/sidebar.php
+++ wp-content/themes/MartinRadmans_laboration1/sidebar.php
@@ -1,11 +1,13 @@
-<aside id="sidebar">
+<aside id="secondary" class="col-xs-12 col-md-3">
   <?php if (is_page(array('undersida', 'undersida-2'))) : ?>
-    <nav>
+    <ul id="menu-sidomeny" class="side-menu">
       <?php wp_nav_menu(array(
         'theme_location' => 'pagemenu',
-        'menu_class' => 'menu'
+        'menu_class' => 'menu',
+        'container' => false,
+        'items_wrap' => '%3$s' // This removes the <ul> wrapper added by wp_nav_menu
       ));
       ?>
-    </nav>
+    </ul>
   <?php endif; ?>
 </aside>
